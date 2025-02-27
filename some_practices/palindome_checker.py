7d620202ace6eef76cb653e59c5c314b76dc5e15
AmandaWu <amandahxff@gmail.com>
2025-02-27 17:28:23 +0800

    palindome_checker.py is used to check whether a phrase is a palindome.

--- some_practices/palindome_checker.py
+++ some_practices/palindome_checker.py
@@ -1,8 +1,23 @@
-#print a multiplication table
-for i in range(1,10):
-    for j in range(1,i+1):
-        a = j * i
-        print(f'j * i = {a} ')
+#Check a phrase is a palindome
+import re
+def is_palindome(text):
+    plain_text = re.sub(r'[^A-Za-z0-9\u4e00-\u9fff]','',text).lower()
+    return plain_text.lower()== plain_text[::-1].lower()
+while True:
+    phrase = input('Enter a phrase to check: ')
+    if phrase.lower() == 'done': 
+        print('\nGoodbye!\n')
+        break
+    elif is_palindome(phrase):
+        print(f'{phrase} is a palindome.\n')
+    else:
+        print(f'{phrase} is not a palindome\n')
+    done_not=input('Do you wanna check more words?(Y/N): ')
+    if done_not.lower() == 'y': continue
+    else: 
+        print('\nGoodbye!\n')
+        break
+
         
                         
                         
