f477f406d0fe817939e57a0917cdd1d1b2dabced
AmandaWu <amandahxff@gmail.com>
2025-02-27 15:37:41 +0800

    the arg of print is changed to end = \t

--- some_practices/Multiplication_table.py
+++ some_practices/Multiplication_table.py
@@ -2,7 +2,8 @@
 for i in range(1,10):
     for j in range(1,i+1):
         a = j * i
-        print(f'j * i = {a} ')
+        print(f'{j} * {i} = {a} ',end = '\t')
+    print()
         
                         
                         
