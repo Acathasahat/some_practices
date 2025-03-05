69e2009dd8a57bfffdd50e8d519df5858da4ff66
AmandaWu <amandahxff@gmail.com>
2025-03-05 18:32:13 +0800

    create_csv.py can create a new csv file and then print it out in two ways

--- some_practices/create_csv.py
+++ some_practices/create_csv.py
@@ -1,10 +1,33 @@
 #to creat a csv file and read it
 import csv
+def dictionary(**kwargs):
+    dic = {}
+    for key,value in kwargs.items():
+        dic[key] = value
+    return dic
 file_name = input('Create a new CSV file (filename.csv): ')
 if not file_name.endswith('.csv'):
     file_name += '.csv'
-with open(file_name,'w',encoding='utf-8',newline='') as f:
+with open(file_name,'w+',encoding='utf-8',newline='') as f:
     fieldnames = ['Name','Age','City']
-    writer = csv.DictWriter(f,fieldnames = fieldnames)
+    writer = csv.DictWriter(f,fieldnames = fieldnames) #DictWriter()创建了一个对象
     writer.writeheader()
-    writer.writerow({'Name':'Alice','Age':'30','City':'New York'})
+    while True:
+        row_d = input('Enter the data in the order of name, age and city: ')
+        if row_d.lower() == 'done': break
+        try:
+            name,age,city=row_d.split(',')
+            row_data = dictionary(Name=name.strip(),Age= age.strip(),City=city.strip())
+        except ValueError:
+            print('Enter the datas in the order of name, age and city. e.g. Alice, 33, New York ')
+            continue
+        writer.writerow(row_data)
+        print(f'\nIf you finished, please enter \'done\'\n')
+    f.seek(0)#将文件指针移动到开头的位置
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
+    f.seek(0)
+    reader= csv.DictReader(f)
+    for row in reader:
+        print(row['Name'],row['Age'],row['City'])
